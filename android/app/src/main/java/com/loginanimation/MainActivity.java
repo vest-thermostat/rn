7c77d14574244b6adfc5c54e07e6090a34218335
Perale Thomas <thomas.perale@openmailbox.org>
2017-03-14 15:10:24 +0100

    Fix android bug

--- android/app/src/main/java/com/loginanimation/MainActivity.java
+++ android/app/src/main/java/com/loginanimation/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends ReactActivity {
      * This is used to schedule rendering of the component.
      */
     @Override
-    protected String getMainComponentName() {
+    public String getMainComponentName() {
         return "LoginAnimation";
     }
 }
