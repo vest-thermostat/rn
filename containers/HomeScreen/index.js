eac501c7ca40d56baef7d80bfe3534684be59afa
Perale Thomas <thomas.perale@openmailbox.org>
2017-03-18 18:00:59 +0100

    Location sending to the server.

--- containers/HomeScreen/index.js
+++ containers/HomeScreen/index.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { AsyncStorage } from 'react-native'
 import { NativeRouter, Route } from 'react-router-native'
+import { Location } from 'expo';
+import axios from 'axios';
 import { Container, Header, Title, Content, Drawer, Button, Left, Right, Body, Icon } from 'native-base';
+
 import SideBar from '../../components/Sidebar';
 
 import HomeContent from './content';
+import SettingsContent from './settings'
 import ScanContent from './scan';
 
 export default class HomeScreen extends Component {
@@ -18,6 +23,39 @@ export default class HomeScreen extends Component {
     openDrawer = () => {
       this._drawer._root.open()
     };
+
+    async componentWillMount () {
+        try {
+            this.token = await AsyncStorage.getItem('token')
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    async componentDidMount () {
+        Location.watchPositionAsync({
+            timeInterval: 300000,
+        }, (coords) =>{
+            axios.post('http://vest.tperale.be/location/', {
+                "position": {
+                    "type": "Point",
+                    "coordinates": [coords.longitude, coords.latitude],
+                }
+            }, {
+                headers: {
+                    'Authorization': 'Token ' + this.token,
+                },
+            }).then(r => {
+                return
+            }).catch(e => {
+                if (e.response) {
+                    console.error(e.response.data);
+                } else {
+                    console.error('Error', e.message);
+                }
+            });
+        });
+    }
 
     render() {
         return (
@@ -46,7 +84,8 @@ export default class HomeScreen extends Component {
                         </Header>
 
                         <Content>
-                            <Route path="/" component={HomeContent}/>
+                            <Route path="/" component={SettingsContent}/>
+                            <Route path="/home" component={HomeContent}/>
                         </Content>
                     </Drawer>
                 </Container>
