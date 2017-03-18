fe55e1cbc016c200a5b071b26eff065f314d1dc0
Perale Thomas <thomas.perale@openmailbox.org>
2017-03-18 18:03:48 +0100

    Code refactoring

--- containers/HomeScreen/index.js
+++ containers/HomeScreen/index.js
@@ -12,84 +12,84 @@ import SettingsContent from './settings'
 import ScanContent from './scan';
 
 export default class HomeScreen extends Component {
-    static propTypes = {
-      logout: React.PropTypes.func,    
+  static propTypes = {
+    logout: React.PropTypes.func,    
+  }
+
+  closeDrawer = () => {
+    this._drawer._root.close()
+  };
+
+  openDrawer = () => {
+    this._drawer._root.open()
+  };
+
+  async componentWillMount () {
+    try {
+      this.token = await AsyncStorage.getItem('token')
+    } catch (e) {
+      console.error(e);
     }
+  }
 
-    closeDrawer = () => {
-      this._drawer._root.close()
-    };
-
-    openDrawer = () => {
-      this._drawer._root.open()
-    };
-
-    async componentWillMount () {
-        try {
-            this.token = await AsyncStorage.getItem('token')
-        } catch (e) {
-            console.error(e);
+  async componentDidMount () {
+    Location.watchPositionAsync({
+      timeInterval: 300000,
+    }, (coords) =>{
+      axios.post('http://vest.tperale.be/location/', {
+        "position": {
+          "type": "Point",
+          "coordinates": [coords.longitude, coords.latitude],
         }
-    }
+      }, {
+        headers: {
+          'Authorization': 'Token ' + this.token,
+        },
+      }).then(r => {
+        return
+      }).catch(e => {
+        if (e.response) {
+          console.error(e.response.data);
+        } else {
+          console.error('Error', e.message);
+        }
+      });
+    });
+  }
 
-    async componentDidMount () {
-        Location.watchPositionAsync({
-            timeInterval: 300000,
-        }, (coords) =>{
-            axios.post('http://vest.tperale.be/location/', {
-                "position": {
-                    "type": "Point",
-                    "coordinates": [coords.longitude, coords.latitude],
-                }
-            }, {
-                headers: {
-                    'Authorization': 'Token ' + this.token,
-                },
-            }).then(r => {
-                return
-            }).catch(e => {
-                if (e.response) {
-                    console.error(e.response.data);
-                } else {
-                    console.error('Error', e.message);
-                }
-            });
-        });
-    }
+  render() {
+    return (
+      <NativeRouter>
+        <Container>
+          <Drawer
+            ref={ref => { this._drawer = ref; }}
+            content={
+                <SideBar
+                    navigator={this._navigator}
+                    logout={this.props.logout}
+                />
+            }
+            onClose={this.closeDrawer.bind(this)}
+          >
+            <Header>
+              <Left>
+                <Button onPress={this.openDrawer.bind(this)} transparent>
+                    <Icon name='menu' />
+                </Button>
+              </Left>
+              <Body>
+                <Title>VEST</Title>
+              </Body>
+              <Right />
+            </Header>
 
-    render() {
-        return (
-            <NativeRouter>
-                <Container>
-                    <Drawer
-                        ref={ref => { this._drawer = ref; }}
-                        content={
-                            <SideBar
-                                navigator={this._navigator}
-                                logout={this.props.logout}
-                            />
-                        }
-                        onClose={this.closeDrawer.bind(this)}
-                    >
-                        <Header>
-                            <Left>
-                                <Button onPress={this.openDrawer.bind(this)} transparent>
-                                    <Icon name='menu' />
-                                </Button>
-                            </Left>
-                            <Body>
-                                <Title>VEST</Title>
-                            </Body>
-                            <Right />
-                        </Header>
-
-                        <Content>
-                            <Route path="/" component={SettingsContent}/>
-                            <Route path="/home" component={HomeContent}/>
-                        </Content>
-                    </Drawer>
-                </Container>
-            </NativeRouter>
-        );
-    }
+            <Content>
+              <Route path="/" component={SettingsContent}/>
+              <Route path="/home" component={HomeContent}/>
+            </Content>
+          </Drawer>
+        </Container>
+      </NativeRouter>
+    );
+  }
 }
